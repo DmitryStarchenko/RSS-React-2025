c5f59d0bbb8ff92b727386a70dd98a0464928b5e
Dmitry <Dololob92@gmail.com>
2025-07-22 14:31:37 +0300

    feat: footer converted to a functional component

--- class-components/src/page/footer/footer.tsx
+++ class-components/src/page/footer/footer.tsx
@@ -1,24 +1,17 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import styles from './footer.module.css';
 
-export class Footer extends Component {
-  state = {
-    isError: false,
-  };
+export function Footer() {
+  const [isError, setIsError] = useState(false);
 
-  render() {
-    if (this.state.isError) {
-      throw new Error('Произошла ошибка при нажатии на кнопку!');
-    }
-    return (
-      <div className={styles.footer}>
-        <button
-          className={styles.errorButton}
-          onClick={() => this.setState({ isError: true })}
-        >
-          Throw Error
-        </button>
-      </div>
-    );
+  if (isError) {
+    throw new Error('Произошла ошибка при нажатии на кнопку!');
   }
+  return (
+    <div className={styles.footer}>
+      <button className={styles.errorButton} onClick={() => setIsError(true)}>
+        Throw Error
+      </button>
+    </div>
+  );
 }
