a09e143cb33c7cbd7936679977856bf3435dd96f
Dmitry <Dololob92@gmail.com>
2025-07-22 14:29:10 +0300

    feat: header converted to a functional component

--- class-components/src/page/header/header.tsx
+++ class-components/src/page/header/header.tsx
@@ -1,46 +1,41 @@
-import { Component, ReactNode } from 'react';
+import { useState } from 'react';
 import styles from './header.module.css';
 
-type Props = {
-  searchRequest: (searchString: string) => void;
-};
+export function Header({
+  searchRequest,
+}: {
+  searchRequest: (search: string) => void;
+}) {
+  const [search, setSearch] = useState('');
 
-export class Header extends Component<Props> {
-  state = {
-    search: '',
-  };
-
-  render(): ReactNode {
-    const idItem = localStorage.getItem('key');
-    return (
-      <header className={styles.header}>
-        <img
-          className={styles.mainLogo}
-          src="../../public/assets/Pikachu.webp"
-          alt="pikachu"
+  const idItem = localStorage.getItem('key');
+  return (
+    <header className={styles.header}>
+      <img
+        className={styles.mainLogo}
+        src="../../public/assets/Pikachu.webp"
+        alt="pikachu"
+      />
+      <div className={styles.headerContent}>
+        <input
+          className={styles.input}
+          type="search"
+          placeholder={
+            idItem ? `Opened card ${idItem}` : 'Enter ID (0-649) or name'
+          }
+          onChange={(event) => {
+            setSearch(event.target.value);
+          }}
         />
-        <div className={styles.headerContent}>
-          <input
-            className={styles.input}
-            type="search"
-            placeholder={
-              idItem ? `Opened card ${idItem}` : 'Enter ID (0-649) or name'
-            }
-            onChange={(event) => {
-              this.setState({ search: event.target.value });
-            }}
-          />
-          <button
-            className={styles.buttonSearch}
-            type="submit"
-            onClick={() => {
-              this.props.searchRequest(this.state.search);
-            }}
-          >
-            Search
-          </button>
-        </div>
-      </header>
-    );
-  }
+        <button
+          className={styles.buttonSearch}
+          type="submit"
+          onClick={() => {
+            searchRequest(search);
+          }}>
+          Search
+        </button>
+      </div>
+    </header>
+  );
 }
