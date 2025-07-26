dc3f76b09aceefaa07509fc3b1a5d08afc37e1a4
Dmitry <Dololob92@gmail.com>
2025-07-27 01:15:44 +0300

    feat: added about content

--- class-components/src/pages/about/about.tsx
+++ class-components/src/pages/about/about.tsx
@@ -4,14 +4,8 @@ export function About() {
   return (
     <main className={styles.aboutMain}>
       <div className={styles.content}>
-        <div className={styles.ghConteiner}>
-          <a
-            className={styles.gh}
-            href="https://github.com/DmitryStarchenko"></a>
-        </div>
-        <div className={styles.rsConteiner}>
-          <a className={styles.rs} href="https://rs.school/courses/reactjs"></a>
-        </div>
+        <a className={styles.gh} href="https://github.com/DmitryStarchenko"></a>
+        <a className={styles.rs} href="https://rs.school/courses/reactjs"></a>
       </div>
     </main>
   );
