0238ac0592a7080fdc7fd610cb7c13655ff16147
Dmitry <Dololob92@gmail.com>
2025-07-30 11:49:50 +0300

    fix: delete throw error button

--- Pokemon/src/widgets/footer/footer.tsx
+++ Pokemon/src/widgets/footer/footer.tsx
@@ -1,17 +1,13 @@
-import { useState } from 'react';
 import styles from './footer.module.css';
 
 export function Footer() {
-  const [isError, setIsError] = useState(false);
-
-  if (isError) {
-    throw new Error('Произошла ошибка при нажатии на кнопку!');
-  }
   return (
     <div className={styles.footer}>
-      <button className={styles.errorButton} onClick={() => setIsError(true)}>
-        Throw Error
-      </button>
+      <img
+        className={styles.pokebol}
+        src="../../../assets/pokebol.png"
+        alt="pokebol"
+      />
     </div>
   );
 }
