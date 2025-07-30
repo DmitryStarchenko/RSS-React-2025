bd2540f05f1867a3f019c3fd904bb1bf1c0120cf
Dima <113242492+DmitryStarchenko@users.noreply.github.com>
2025-07-30 11:21:56 +0300

    fix: delete class-components directory

--- class-components/src/shared/index.ts
+++
@@ -1,4 +0,0 @@
-export { useLocalStorage } from './custom-hooks';
-export { apiRequest } from './api';
-export { handleSearchRequest } from './handle-search-request';
-export { CardContextProvider, CardContext } from './context';
